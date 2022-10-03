f3009815f5f8ed758ee362381bfe99725d02a31a
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-10-03 23:49:51 +0700

    add

--- get_next_line.c
+++ get_next_line.c
@@ -3,14 +3,32 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smeethon <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: smeethon <smeethon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 10:40:17 by smeethon          #+#    #+#             */
-/*   Updated: 2022/09/29 10:40:23 by smeethon         ###   ########.fr       */
+/*   Updated: 2022/10/03 23:49:14 by smeethon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-    
+	static char	*size;
+	char		*str;
+
+	if (BUFFER_SIZE < 1 || read(fd, 0, 0) < 0 || fd < 0)
+		return (0);
+	if (!size)
+	{
+		size = malloc(1);
+		size[0] = '\0';
+	}
+	size = read_file (size, fd);
+	if (!size)
+	{
+		free(size);
+		return (0);
+	}
+	str = get_the_line(size);
+	size = plus_line(size);
+	return (str);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,9 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smeethon <smeethon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 23:48:23 by smeethon          #+#    #+#             */
+/*   Updated: 2022/10/03 23:49:16 by smeethon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
 
-char *get_next_line(int fd);
+char	*get_next_line(int fd);
+char	*read_file(char *size, int fd);
 
 #endif
