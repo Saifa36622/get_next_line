179b8cf73c35d8cedf927d69a9c90ccb0030fb07
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2022-12-27 20:10:41 +0700

    wow

--- get_next_line.c
+++ get_next_line.c
@@ -10,25 +10,3 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*get_next_line(int fd)
-{
-	static char	*size;
-	char		*str;
-
-	if (BUFFER_SIZE < 1 || read(fd, 0, 0) < 0 || fd < 0)
-		return (0);
-	if (!size)
-	{
-		size = malloc(1);
-		size[0] = '\0';
-	}
-	size = read_file (size, fd);
-	if (!size)
-	{
-		free(size);
-		return (0);
-	}
-	str = get_the_line(size);
-	size = plus_line(size);
-	return (str);
-}
