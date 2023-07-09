3160ec4129e66e931afa6f93a71967dc43665146
Saifa Meethong <smeethon@z3t5c1.42bangkok.com>
2023-07-09 20:46:10 +0700

    come back

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: smeethon <smeethon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 23:48:23 by smeethon          #+#    #+#             */
-/*   Updated: 2022/12/28 23:21:45 by smeethon         ###   ########.fr       */
+/*   Updated: 2023/07/09 20:45:39 by smeethon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,10 @@
 # include <stdio.h>
 # include <string.h>
 
-char	*get_next_line(int fd);
+
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_strchr(const char *s, int c);
 size_t	ft_strlen(const char *x);
-char	*gen_buffplusread(int   fd, char *ans);
-char    *freetmp(char *ans,char *buff);
+
 
 #endif
